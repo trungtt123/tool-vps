96b909841548e17585020d3b899798d77d57393f
MF Trung.tq <trung.tq@mediafusion.co.jp>
2024-01-25 12:59:59 +0900

    update

--- const.js
+++ const.js
@@ -1,27 +1,16 @@
+require('dotenv').config()
 const FOLDER_PROFILE = 'D:\\Profiles';
 const API_GPM_URL = 'http://127.0.0.1:7070/v1';
 const API_PYTHON_URL = 'http://127.0.0.1:5001';
 const API_NGROK_URL = 'http://127.0.0.1:4040';
 const CONFIG_ROOT = __dirname;
-const DATABASE_URL = __dirname + "\\Database\\database.txt";
-const PROFILES_PATH = __dirname + "\\Database\\Profiles";
-const BUFF_YOUTUBE_SCRIPT_ENUM = [
-    'youtubeSearch_1',
-    'youtubeSearch_2',
-    'browseFeature_1',
-    'channelPage_1',
-    'channelPage_2',
-    'external_1',
-    'directOrUnknown_1',
-    'playlist_1',
-    'notification_1'
-];
+const DATABASE_URL = process.env.DATABASE_URL;
+const PROFILES_PATH = process.env.PROFILES_PATH;
 module.exports = {
     API_PYTHON_URL,
     CONFIG_ROOT,
     API_NGROK_URL,
     FOLDER_PROFILE,
-    BUFF_YOUTUBE_SCRIPT_ENUM,
     API_GPM_URL,
     DATABASE_URL,
     PROFILES_PATH
